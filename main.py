0a336c4dea3a65ebab30528ead17a60d5ee32a86
lgsrocha <lucasgabriels2210@gmail.com>
2025-04-12 03:47:33 -0300

    added syschecks

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import sys
 from stats import get_book_text, word_splitter, character_counter, formated_info
 
-filepath="books/frankenstein.txt"
+
 
 def main():
+    if (len(sys.argv)) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath=sys.argv[1]
     book_text = get_book_text(filepath)
     words_count = word_splitter(book_text, "count")
     words = word_splitter(book_text)
     character_count = character_counter(words)
+    
     print(formated_info(words_count,filepath,character_count))
 
 main()  
