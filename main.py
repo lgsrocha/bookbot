5d9c788648117e43fd499aaa99d1220a44ddc169
lgsrocha <lucasgabriels2210@gmail.com>
2025-04-11 23:38:39 -0300

    added wordcount

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(path):
+    # TODO validate path
+    with open(path) as book:
+        return book.read()
+
+def word_count(string):
+    words = string.split()
+    return len(words)        
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = word_count(book_text)
+    print(f"{num_words} words found in the document")
+    
+main()  
